a1795c1396b4ebe728eb583cc4ace33a37494991
Orion Engleton - MBAir <orion.engleton@gmail.com>
2014-10-24 23:10:31 -0700

    removed cruft

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -31,19 +31,6 @@ def create
     end
   end
 
-  #def create
-  #This logs a user in 
-  #  user = Token.from_omniauth(env["omniauth.auth"])
-  #  if user
-  #    session[:user_id] = user.id
-  #    redirect_to feed_user_path(user), notice: "Signed in!"
-  #  else
-  #    session[:user_id] = nil
-  #    flash.now[:error] = 'Invalid email/password combination'
-  #    render 'new'
-  #  end
-  #end
-
   def destroy
     #This destroys the session a logs a user out
     session[:user_id] = nil
