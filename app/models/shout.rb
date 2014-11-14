5b956c17c44b725f634d40a69f758a96ecec90ca
Ted Martinez <tedma4@yahoo.com>
2014-11-13 19:57:38 -0700

    heroku test

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -7,6 +7,9 @@ class Shout < ActiveRecord::Base
   acts_as_votable
   has_attached_file :pic, :styles => {  :thumb => "600x600#", :medium => "300x300#", :small => "160x160#"}
   validates_attachment_content_type :pic, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  has_attached_file :snip, :styles => {
+                            :mobile => {:geometry => "400x300", :format => 'ogg', :streaming => true}
+                                        }, :processors => [:ffmpeg, :qtfaststart]
 
 	#attr_accessor :content, :photo, :photo_delete, :video, :video_delete, :dependent => :destroy
 	#has_destroyable_file :photo, :video
