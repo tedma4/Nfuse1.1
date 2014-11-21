de24fe441346424d15594bd4b3a157a2b657e3aa
Orion Engleton - MBAir <orion.engleton@gmail.com>
2014-11-20 22:29:40 -0700

    acts as votable hack

--- app/models/shout.rb
+++ app/models/shout.rb
@@ -5,20 +5,25 @@ class Shout < ActiveRecord::Base
   validates :user_id, presence: true
   has_many :comments, dependent: :destroy
 
-  acts_as_votable
+
   has_attached_file :pic, :styles => {  :thumb => "600x600#", :medium => "300x300#", :small => "160x160#"}
   validates_attachment_content_type :pic, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   has_attached_file :snip, :styles => {
                             :mobile => {:geometry => "400x300", :format => 'ogg', :streaming => true}
                                         }, :processors => [:ffmpeg, :qtfaststart]
-
   after_create :this_is_video!
+  acts_as_votable
 	#attr_accessor :content, :photo, :photo_delete, :video, :video_delete, :dependent => :destroy
   has_destroyable_file :pic, :snip
 
   def this_is_video!
     update_attribute(:video, true) if !self.snip_file_name.nil?
   end
+
+  def all_votes
+    ActsAsVotable::Vote.where(votable_id: self.id)
+  end
 end
+
 
 
