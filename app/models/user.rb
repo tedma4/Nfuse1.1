b383a2d92200edfe62486306583907bf85197d26
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-01-24 01:29:27 +1200

    Updated the user model - arranging callbacks, validations and relationships into specific groups

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,41 +1,51 @@
 class User < ActiveRecord::Base
-  
+  # 
+  # Relationships
+  # 
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   has_many :followed_users, through: :relationships, source: :followed
   has_many :reverse_relationships, foreign_key: "followed_id",
                                    class_name:  "Relationship",
                                    dependent:   :destroy
   has_many :followers, through: :reverse_relationships, source: :follower
+  has_many :tokens, dependent: :destroy
+  has_many :conversations, :foreign_key => :sender_id
+  has_many :shouts 
+  has_many :comments
+
+  # 
+  # Callbacks & Macros
+  # 
+  has_secure_password
+  acts_as_voter
+
+  before_create :downcase_email
   before_create :create_remember_token
+
   has_attached_file :avatar, styles: { larger: "280x280#", medium: "300x300#", thumb: "50x50#", followp: "208x208#" }, 
                                 default_url: "default.png"
                                 #:url  => "/assets/images/:id/:style/:basename.:extension",
                                 #:path => ":rails_root/assets/images/:id/:style/:basename.:extension"
-
-  validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
   has_attached_file :banner, styles: { larger: "851x315#", medium: "300x300#" }, 
                                 default_url: "default2.png"
                                 #:url  => "/assets/images/:id/:style/:basename.:extension",
                                 #:path => ":rails_root/assets/images/:id/:style/:basename.:extension"
 
+  # 
+  # Validations
+  # 
+  validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   validates_attachment_content_type :banner, :content_type => ["image/jpg", "image/jpeg", "image/png" ]
-  
   validates :first_name, :last_name, presence: true
-  before_save { self.email = email.downcase }
+  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
-  has_secure_password
-  validates :password, length: {minimum: 6}, allow_blank: true
-  #This allows a user to have multiple oauth tokens
-  has_many :tokens, dependent: :destroy
-  has_many :conversations, :foreign_key => :sender_id
-#This sends the email with the password reset token in it
-  has_many :shouts 
-  has_many :comments
+  
+  validates :email, presence: true,
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
 
-  acts_as_voter
+  validates :password, length: {minimum: 6}, allow_blank: true
 
   def send_password_reset
     generate_token(:password_reset_token)
@@ -103,10 +113,16 @@ class User < ActiveRecord::Base
   def self.search(search)
       where("first_name like :s or last_name like :s or first_name || ' ' || last_name like :s", :s => "%#{search}") 
   end
+
   private
 
-    def create_remember_token
-      self.remember_token = User.digest(User.new_remember_token)
-    end
+  def downcase_email
+    email.downcase
+  end
+
+  def create_remember_token
+    self.remember_token = User.digest(User.new_remember_token)
+  end
+
 end
 
