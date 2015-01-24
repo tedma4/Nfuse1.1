9b8e9bc3014e3c0eba30e33a8eb6986432c3a8c0
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-01-24 14:19:31 +1200

    Refactoring methods out of user class that dont write to db into specific modules

---
+++ app/models/concerns/authentication.rb
@@ -0,0 +1,47 @@
+module Authentication
+  extend ActiveSupport::Concern
+
+  included do
+    has_secure_password
+  end
+
+  def send_password_reset
+    generate_token(:password_reset_token)
+    self.password_reset_sent_at = Time.zone.now
+    save!
+    UserMailer.password_reset(self).deliver
+  end
+
+  #This generates a password reset token
+  def generate_token(column)
+    begin
+      self[column] = SecureRandom.urlsafe_base64
+    end while User.exists?(column => self[column])
+  end
+
+  #This validates the password reset token sent to a user
+  def validate_tokens!
+    tokens.each(&:validate_token!)
+  end
+
+  def new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def digest(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  module ClassMethods
+
+    def new_remember_token
+      SecureRandom.urlsafe_base64
+    end
+
+    def digest(token)
+      Digest::SHA1.hexdigest(token.to_s)
+    end
+
+  end
+
+end
