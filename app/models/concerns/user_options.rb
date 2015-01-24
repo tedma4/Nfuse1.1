ee727ef8ea167514ce4100eda1aa3ced1118bc25
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-01-24 12:40:58 +1200

    methods now in UserOptions module

---
+++ app/models/concerns/user_options.rb
@@ -0,0 +1,31 @@
+module UserOptions
+  extend ActiveSupport::Concern
+  #These are the relationship status options a user can choose from
+  def rel_stat
+    ["I don’t want to say",
+     "Single",
+     "In a relationship",
+     "Engaged",
+     "Married",
+     "It’s complicated",
+     "In an open relationship",
+     "Widowed",
+     "In a domestic partnership",
+     "In a civil union"][self.relationship_status - 1]
+  end
+#These are the interested in options a user can choose from
+  def int_in
+    ["Women", "Men", "Women and Men"][self.interested_in - 1]
+  end
+
+#These are the looking for options a user can choose from
+  def look
+    ["Friends", "Dating", "A relationship", "Networking", "Fun"][self.looking_for - 1]
+  end
+
+#These are the gender options a user can choose from
+  def gender_txt
+    ["Not Telling", "Male", "Female"][self.gender - 1]
+  end
+
+end
