52552463329c0d7b8b2e2ac5e07022537189181a
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-01-24 17:17:56 +1200

    Setup factory for Relationship - added basic specs to test the relationship

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,5 +3,4 @@ class Relationship < ActiveRecord::Base
   belongs_to :followed, class_name: "User"
   validates :follower_id, presence: true
   validates :followed_id, presence: true
-
 end
--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Relationship do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'be associated with User class' do
+    relationship = Relationship.new(followed: nil)
+    expect(relationship).not_to be_valid
+  end
+
+  it 'two new users build a relationship' do
+    relationship = build(:relationship)
+    expect(relationship).to be_valid
+    relationship.save
+
+    expect(Relationship.count).not_to be 0
+  end
+
 end
