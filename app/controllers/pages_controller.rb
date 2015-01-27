8ada7c6b1bc03d450aad8037523f6b9e40855f5d
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-01-27 20:21:10 +1200

    Discovered a error in the logic  of the each loop that sets the feed.  Everytime the main variables are being overwritten only the last user in the loops data - was set previously - turned them into arrays LIKE timeline and we can decide to do something with it later - probably iterate over them as well.  Moved logic to timeline utility method.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,25 +1,42 @@
 class PagesController < ApplicationController
-  
-  #before_filter :signed_in_user, only: [:home]
 
   def home
     if signed_in?
-      @user = User.find(session[:user_id])
-      @providers = Providers.for(@user)
-      timeline = []
+      @providers = Providers.for(current_user)
+      @timeline  = timeline[:timeline].flatten.sort {|a, b|  b.created_time <=> a.created_time }
+      # We need to do something with this
+      # because if each user's details is needed.
+      # only the last user's data is saved to this loop.
+      # hence why i put #first at the end of the hash.
 
-      current_user.followed_users.each do |user|
-        feed=Feed.new(user)
-        timeline << feed.construct(params)
-        @unauthed_accounts               = feed.unauthed_accounts
-        @poster_recipient_profile_hash   = feed.poster_recipient_profile_hash
-        @commenter_profile_hash          = feed.commenter_profile_hash
-      end
-
-      @timeline=timeline.flatten.sort {|a, b|  b.created_time <=> a.created_time }
+      @unauthed_accounts              = timeline[:unauthed_accounts].first
+      @poster_recipient_profile_hash  = timeline[:poster_recipient_profile_hash].first
+      @commenter_profile_hash         = timeline[:commenter_profile_hash].first
     end
-      render "home"
+    # render 'home' is implicit.
   end
+
+  private
+
+  def timeline(user=current_user)
+    stack = {
+      timeline: [],
+      unauthed_accounts: [],
+      poster_recipient_profile_hash: [],
+      commenter_profile_hash: []
+    }
+    current_user.followed_users.each do |user|
+      feed=Feed.new(user)
+      stack[:timeline] << feed.construct(params)
+      # this is constantly getting over written for each user.
+      stack[:feed_unauthed_accounts] << feed.unauthed_accounts
+      stack[:poster_recipient_profile_hash] << feed.poster_recipient_profile_hash
+      stack[:commenter_profile_hash] << feed.commenter_profile_hash
+    end
+    stack
+  end
+
+  public
 
   def help; end
   def about; end
