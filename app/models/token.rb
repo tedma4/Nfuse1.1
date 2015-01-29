00cf26df5403b2687c538dc805a8311575b46af5
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-01-29 14:26:55 +1200

    Created factory for token - cleaned up the token class methods will refactor some more - tests pass.

--- app/models/token.rb
+++ app/models/token.rb
@@ -10,43 +10,27 @@ class Token < ActiveRecord::Base
   end
 
   def self.update_or_create_with_twitter_omniauth(id, auth)
-    token = where(provider: auth["provider"], uid: auth["uid"]).first_or_initialize
-    token.provider = auth["provider"]
-    token.uid = auth["uid"]
-    token.access_token = auth["extra"]["access_token"].token
-    token.access_token_secret = auth["extra"]["access_token"].secret
-    token.user_id = id
-    token.save!
-    token
+    build_token(id, auth)
+    @token.access_token        = auth["extra"]["access_token"].token
+    @token.access_token_secret = auth["extra"]["access_token"].secret
+    @token.save!
+    @token
   end
 
   def self.update_or_create_with_other_omniauth(id, auth)
-    token = where(provider: auth["provider"], uid: auth["uid"]).first_or_initialize
-    token.provider = auth["provider"]
-    token.uid = auth["uid"]
-    token.access_token = auth["credentials"]["token"]
-    token.user_id = id
-    token.save!
-    token
+    build_token(id, auth)
+    @token.access_token       = auth["credentials"]["token"]
+    @token.save!
+    @token
   end
 
-  #def self.find_with_omniauth(auth)
-  #  find_by_provider_and_uid(auth['provider'], auth['uid'])
-  #end
-
-  #def self.create_with_omniauth(auth)
-  #  create(uid: auth['uid'], provider: auth['provider']) # and other data you might want from the auth hash
-  #end
-
-  #def self.update_or_create_with_omniauth(user, auth)
-  #  token = where(provider: auth["provider"], uid: auth['uid']).first_or_initialize
-  #  token.provider = auth["provider"]
-  #  token.uid = auth["uid"]
-  #  token.access_token = auth["credentials"]["token"]
-  #  token.user_id = user.id
-  #  token.save!
-  #  token
-  #end
+  def self.build_token(id, auth)
+    @token ||= where(provider: auth["provider"], uid: auth["uid"]).first_or_initialize
+    @token.provider = auth["provider"]
+    @token.uid = auth["uid"]
+    @token.user_id = id
+    @token
+  end
 
   def configure_twitter(access_token, access_token_secret)
     client = Twitter::REST::Client.new do |config|
--- spec/models/token_spec.rb
+++ spec/models/token_spec.rb
@@ -1,5 +1,39 @@
 require 'spec_helper'
 
 describe Token do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Associations
+  it { is_expected.to belong_to :user }
+  # Validations
+  it { is_expected.to validate_presence_of :uid}
+  it { is_expected.to validate_presence_of :provider }
+
+  describe 'custom queries' do
+    it 'Token.by_name' do
+      expect(Token.by_name('string')).to be_a ActiveRecord::Relation
+    end
+
+    it 'can find or create token' do
+
+      auth_hash = attributes_for(:token)
+      # mock twitter_hash 
+      AToken = Struct.new(:x) do 
+        def token 
+          SecureRandom.hex(20)
+        end
+        def secret
+          SecureRandom.hex(20)
+        end
+      end
+      
+    mock_hash = {
+        'provider' => 'twitter',
+         'uid' => SecureRandom.hex(10),
+         'extra' => { 'access_token' =>  AToken.new },
+         'credentials' => {'token' => AToken.new.token }
+      }
+      other_user = create(:user)
+      expect(Token.update_or_create_with_twitter_omniauth(other_user.id, mock_hash)).to be_a Token
+      expect(Token.update_or_create_with_other_omniauth(other_user.id, mock_hash)).to be_a Token
+    end
+  end
 end
