af78170f8700aed95781f67c2369cac133c30037
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-02-04 23:47:03 +1200

    removed commented out code

--- spec/models/twitter/api_spec.rb
+++ spec/models/twitter/api_spec.rb
@@ -1,13 +1,5 @@
 require 'spec_helper'
 
-describe  Twitter::Post do
-
-  it 'should include Twitter::Api' do
-    expect(Twitter).to have_constant(:Api)
-  end
-
-end
-
 describe Twitter::Api do
   # 
 end
