4cc4af4d85a6498681ccf7ce16e64491c7131043
Ted Martinez <tedma4@yahoo.com>
2015-03-05 12:59:23 -0700

    Updated the nfuse page view and added comment class to nfuse module

---
+++ app/models/nfuse/comment.rb
@@ -0,0 +1,33 @@
+module Nfuse
+  class Comment
+    delegate :created_at, :user, :body, :id, to: :comment
+
+    attr_reader :provider
+    attr_accessor :comment
+
+    def initialize(comment)
+      @comment = comment
+      @provider = "nfuse"
+    end
+
+    def created_time
+      @comment.created_at
+    end
+    
+    def avatar
+      @comment.user.avatar
+    end
+
+    def profile
+      @comment.user
+    end
+
+    def full_name
+      @comment.user.full_name
+    end
+
+    def body
+      @comment.body
+    end
+  end
+end
