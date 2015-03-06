23db1a59a8dc14cf388ef10ef7829471ea2a24b4
Ted Martinez <tedma4@yahoo.com>
2015-03-05 21:46:22 -0700

    Added nfuse username to twitter and fixed heroku problem

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -72,9 +72,9 @@ class CommentsController < ApplicationController
 
   def find_commentable
     params.each do |name, value|
-        if name =~ /(.+)_id$/
-            return $1.classify.constantize.find(value)
-        end
-    end || raise ActiveRecord:NoRecord.new("Couldn\'t find it captain!")
+      if name =~ /(.+)_id$/
+          return $1.classify.constantize.find(value)
+      end #|| raise ActiveRecord:NoRecord.new("Couldn\'t find it captain!")
+    end 
   end
 end
