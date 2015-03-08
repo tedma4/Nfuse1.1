388922f6854a37fe65abded78960197ae49f8b60
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-03-07 17:39:06 -0700

    Updatedd the nfuse page model - delegated some methods to the shout model

--- app/models/nfuse_page.rb
+++ app/models/nfuse_page.rb
@@ -1,6 +1,8 @@
 class NfusePage < ActiveRecord::Base
   has_many :shouts
   belongs_to :user
+
+  delegate :url, :is_pic?, :pic, :url_html , to: :other_shout
    
   def self.my_nfuse_page(user_id)
     self.where(user_id: user_id)
@@ -8,5 +10,14 @@ class NfusePage < ActiveRecord::Base
   
   def self.nfuse_page_owner(user_id)
     User.find_by id: user_id
-  end  
+  end
+
+  def other_shout
+    @shout = Shout.find(self.shout_id)
+  end
+
+  def url_html
+    other_shout.url_html
+  end
+
 end
