059b84322ba800ea3a387d8d8dd3762d7affbc6c
Ted Martinez <tedma4@yahoo.com>
2015-03-11 17:46:33 -0700

    One more Time to fix the bigint issue

--- db/migrate/20150311224117_change_migration_back_to_bigint.rb
+++ db/migrate/20150311224117_change_migration_back_to_bigint.rb
@@ -1,17 +1,17 @@
 class ChangeMigrationBackToBigint < ActiveRecord::Migration
   def self.up
-    # execute <<-SQL
-    #   ALTER TABLE votes
-    #   ALTER COLUMN votable_id TYPE bigint USING votable_id::bigint
-    # SQL
-    change_column :votes, :votable_id, :bigint
+     execute <<-SQL
+       ALTER TABLE votes
+       ALTER COLUMN votable_id TYPE bigint USING votable_id::bigint
+     SQL
+    #change_column :votes, :votable_id, :bigint
   end
 
   def self.down
-    change_column :votes, :votable_id, :string
-    # execute <<-SQL
-    #   ALTER TABLE votes
-    #   ALTER COLUMN votable_id TYPE string
-    # SQL
+    #change_column :votes, :votable_id, :string
+     execute <<-SQL
+       ALTER TABLE votes
+       ALTER COLUMN votable_id TYPE string
+     SQL
   end
 end
