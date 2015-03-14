1b33b6165c2657aa1ab069ebcb312f4f252a6a72
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-03-14 02:03:54 -0700

    Removed delegating to a shout object that we no longer need or reference

--- app/models/nfuse_page.rb
+++ app/models/nfuse_page.rb
@@ -3,7 +3,7 @@ class NfusePage < ActiveRecord::Base
   #has_many :shouts, through: :users
   belongs_to :user
 
-  delegate :url, :is_pic?, :pic, :url_html , to: :other_shout
+  # attr_accessor :url_html
    
   def self.my_nfuse_page(user_id)
     self.where(user_id: user_id)
@@ -13,12 +13,29 @@ class NfusePage < ActiveRecord::Base
     User.find_by id: user_id
   end
 
-  def other_shout
-    @shout = Shout.find(self.shout_id)
+  def self.build(options)
+
+    nfuse_page = new( social_key: options[:key] || 'nfuse',
+                      social_id: options[:id],
+                      url: options[:url],
+                      owner_id: options[:owner_id],
+                      user_id: options[:user_id] )
+    nfuse_page.save
+    nfuse_page
   end
 
-  def url_html
-    other_shout.url_html
+  def is_pic?
+    !url.blank?
   end
 
+  auto_html_for :url do
+    html_escape
+    image
+    youtube(:width => '100%', :height => 225, :autoplay => false)
+    vimeo(:width => '100%', :height => 225, :autoplay => false)
+    soundcloud
+    link :target => "_blank", :rel => "nofollow"
+    simple_format
+  end
+  
 end
