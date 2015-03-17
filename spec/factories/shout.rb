14deecc3221180d3788692351e4a1bf050b41155
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-03-16 20:59:12 -0700

    Updated factory

--- spec/factories/shout.rb
+++ spec/factories/shout.rb
@@ -3,9 +3,5 @@ FactoryGirl.define do
    # belongs to a User
    association :user, factory: :user
    content 'This is content for the shout - the text string.'
-
-   # factory :shout_with_picture do
-   #  pic Rack::Test::UploadedFile.new("/#{Rails.root.to_s}/spec/factories/images/one.jpg", 'image/jpg')
-   # end
   end
 end
