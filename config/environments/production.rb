8525bbd4a96055a209900628c2df3df20df93ca4
Ted Martinez <tedma4@yahoo.com>
2015-03-25 09:30:03 -0700

    Updated callback links and network logos

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,8 +76,21 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.action_mailer.default_url_options = { :host => "nfuse.it" }
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif] 
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :enable_starttls_auto => true,
+      :domain          => "gmail.com",
+      :address         => 'smtp.gmail.com',
+      :port            =>  587,
+      :authentication  => :plain,
+      :user_name       => 'tedma4@gmail.com',
+      :password        => 'tm671216'
+
+  }
 end
