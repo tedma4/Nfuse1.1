881ddab71732b81cb6f97be68c06bb3833847292
Ted Martinez <tedma4@yahoo.com>
2015-04-06 14:50:21 -0700

    Updated the oauth method for the flickr controller

--- app/controllers/registrations/flickr_controller.rb
+++ app/controllers/registrations/flickr_controller.rb
@@ -4,7 +4,7 @@ class Registrations::FlickrController < ApplicationController
     #The omniauth used to authorize a facebook user's posts
     auth = request.env["omniauth.auth"]
     user = User.find(session[:user_id])
-    Token.update_or_create_with_other_omniauth(user.id, auth)
+    Token.update_or_create_with_flickr_omniauth(user.id, auth)
     #redirect_to feed_user_path(user)
     redirect_to request.env['omniauth.origin'] || '/default' # callback_links_path(user)
   end
