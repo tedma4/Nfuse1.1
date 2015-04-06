519110e85c398814942d2771e569f1b19ce0b2ad
Ted Martinez <tedma4@yahoo.com>
2015-04-06 14:53:25 -0700

    Updated the flicker timeline and added the post partials to the feed_content partial

--- app/models/flickr/timeline.rb
+++ app/models/flickr/timeline.rb
@@ -1,6 +1,20 @@
 module FLickr
   class Timeline
 
+    # FlickRaw.api_key="... Your API key ..."
+    # FlickRaw.shared_secret="... Your shared secret ..."
+
+    # list   = flickr.photos.getRecent
+
+    # id     = list[0].id
+    # secret = list[0].secret
+    # info = flickr.photos.getInfo :photo_id => id, :secret => secret
+
+    # sizes = flickr.photos.getSizes :photo_id => id
+
+    # original = sizes.find {|s| s.label == 'Original' }
+    # puts original.width       # => "226" -- may fail if they have no original marked image
+
     attr_reader :authed, :last_vid_id
 
     def initialize(user=current_user)
@@ -29,7 +43,7 @@ module FLickr
     end
 
     def configure_flickr(tokens)
-      @config ||= tokens.configure_flickr(tokens.access_token, tokens.refresh_token)#, tokens.expiresat)
+      @config ||= tokens.configure_flickr(tokens.access_token, tokens.access_token_secret)#, tokens.expiresat)
     end
 
     def get_timeline(client, max_id)
@@ -51,3 +65,21 @@ module FLickr
     end
   end
 end
+
+# FlickRaw.api_key="... Your API key ..."
+# FlickRaw.shared_secret="... Your shared secret ..."
+
+# token = flickr.get_request_token
+# auth_url = flickr.get_authorize_url(token['oauth_token'], :perms => 'delete')
+
+# puts "Open this url in your process to complete the authication process : #{auth_url}"
+# puts "Copy here the number given when you complete the process."
+# verify = gets.strip
+
+# begin
+#   flickr.get_access_token(token['oauth_token'], token['oauth_token_secret'], verify)
+#   login = flickr.test.login
+#   puts "You are now authenticated as #{login.username} with token #{flickr.access_token} and secret #{flickr.access_secret}"
+# rescue FlickRaw::FailedResponse => e
+#   puts "Authentication failed : #{e.msg}"
+# end
