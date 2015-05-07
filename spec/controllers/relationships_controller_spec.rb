7bb8101e05cae291f55252ce861bfebb56d85e41
MattHamada <mattahamada@gmail.com>
2015-05-06 18:02:43 -0700

    fixed RelationshipsController tests

--- spec/controllers/relationships_controller_spec.rb
+++ spec/controllers/relationships_controller_spec.rb
@@ -1,22 +1,32 @@
 require 'spec_helper'
 
 describe RelationshipsController, type: :controller do
-
+  let(:user) {create(:user)}
+  let(:related_user) {create(:user)}
   before(:each) do
+    user.save
+    related_user.save
     login create(:user)
   end
 
-  describe "GET 'create'" do
-    xit "returns http success" do
-      get 'create'
-      response.should be_success
+  describe "POST 'create'" do
+    it "returns http success" do
+      post 'create', {relationship: {followed_id: related_user.id}}
+      expect(response.status).to eq 302
+    end
+    it 'should increase the users relationship count' do
+      expect{post 'create', {relationship: {followed_id: related_user.id}}}.to change(current_user.relationships, :count).by(1)
     end
   end
 
   describe "GET 'destroy'" do
-    xit "returns http success" do
-      get 'destroy'
-      response.should be_success
+    before {post 'create', {relationship: {followed_id: related_user.id}}}
+    it "returns http success" do
+      delete 'destroy', {id: Relationship.last.id}
+      expect(response.status).to eq 302
+    end
+    it 'should decrease the users relationship count' do
+      expect{delete 'destroy', {id: Relationship.last.id}}.to change(current_user.relationships, :count).by(-1)
     end
   end
 
