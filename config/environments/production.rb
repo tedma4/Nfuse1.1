eedb3b05c302a8852610737fc30b7529d60c1cc3
Ted Martinez <tedma4@yahoo.com>
2015-05-17 18:21:03 -0700

    Active admin regex added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,8 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  config.assets.precompile +=  %w( *.js *.css.scss *.scss *.css *.scss.erb *.css.erb )
+  config.assets.precompile +=  %w( *.css.scss *.scss *.scss.erb *.css.erb )
+  config.assets.precompile << /\A(?!active_admin).*\.(js|css)\z/
   # Generate digests for assets URLs.
   config.assets.digest = true
 
