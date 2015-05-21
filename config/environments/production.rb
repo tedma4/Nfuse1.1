5eaacc06ee14053ba919a359c701588310790d93
Ted Martinez <tedma4@yahoo.com>
2015-05-21 09:27:17 -0700

    Added s3 host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,8 +99,9 @@ config.assets.precompile += %w( video-js.swf vjs.eot vjs.svg vjs.ttf vjs.woff )
  
   # Sets Paperclip to upload images to Amazon S3
   config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
+    storage: :s3,
+    s3_host_name: 's3-us-west-2.amazonaws.com',
+    s3_credentials: {
       bucket: ENV["AWS_BUCKET_NAME"],
       access_key_id: ENV["AWS_ACCESS_KEY_ID"],
       secret_access_key: ENV["AWS_SECRET_KEY"]
