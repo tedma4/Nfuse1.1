90c5cfac1c965f99978ad99ca70fbbedc99afada
tedma4 <tedma4@yahoo.com>
2015-06-23 14:16:15 -0700

    Updated paperclip path

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,8 +34,8 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
-  Paperclip.options[:command_path] = "C:/Program Files/ImageMagick-6.8.9-Q16/convert.exe"
-  Paperclip.options[:command_path] = "C:/Program Files/ImageMagick-6.8.9-Q16/ffmpeg.exe"
+  Paperclip.options[:command_path] = "C:/Program Files/ImageMagick-6.9.1-Q16/convert.exe"
+  Paperclip.options[:command_path] = "C:/Program Files/ImageMagick-6.9.1-Q16/ffmpeg.exe"
   # Paperclip.options[:command_path] = "C:/ffmpeg/bin/ffmpeg.exe"
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
