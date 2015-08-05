b91b98f52bce4e195d67291cdfa61acfee5f0642
tedma4 <tedma4@yahoo.com>
2015-08-05 11:59:03 -0700

    Finished Fb

--- app/models/facebook/post.rb
+++ app/models/facebook/post.rb
@@ -20,10 +20,6 @@ module Facebook
       ActsAsVotable::Vote.where(votable_id: id).count
     end
 
-    def id
-      @post.id
-    end
-
     # User Object * because delegate is not working.
 
     def avatar
@@ -35,7 +31,7 @@ module Facebook
     end
 
     def link_to_post
-      "https://facebook.com/#{username}/status/#{id}"
+      @post['actions'][0]['link']
     end
 
     def created_time
@@ -50,40 +46,44 @@ module Facebook
       @post['id']
     end
 
+    def source
+      @post['source']
+    end
+
+    def youtube_source
+      @post['source'].sub("?autoplay=1", "")
+    end
+
     def post_user
       @post.user
     end
 
-  def story
-    @post['story']
-  end
-
-  def story_tags
-    @post['story_tags']#.map { |story_tag| StoryTag.from(story_tag) }
-  end
-
-  def caption_text
-    @post['caption']
-  end
-
-  def article_link
-    @post['link']
-  end
-
-    def post_video
-      @post.attrs[:extended_entities][:media][0][:video_info][:variants][2][:url]
+    def article_link
+      @post['link']
     end
 
-  def type
-    @post['type']
-  end
+    def type
+      @post['type']
+    end
 
-  def status_type
-    @post['status_type']
-  end
+    def status_type
+      @post['status_type']
+    end
   
     def image
       @post['picture']
+    end
+
+    def description
+       @post['description']
+    end
+
+    def message
+      @post['message']
+    end
+
+    def caption
+      @post['caption']
     end
   end
 end
