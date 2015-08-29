de4d4ae1d175f3b64e730c50c8549b9ffa7d8550
tedma4 <tedma4@yahoo.com>
2015-08-29 08:59:39 -0700

    Added bullet gem initializer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,19 @@ Rails.application.configure do
 
   }
 
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    # Bullet.rails_logger = true
+    # Bullet.honeybadger = true
+    # Bullet.bugsnag = true
+    # Bullet.airbrake = true
+    # Bullet.rollbar = true
+    Bullet.add_footer = true
+    #Bullet.stacktrace_includes = [ 'your_gem', 'your_middleware' ]
+    #Bullet.slack = { webhook_url: 'http://some.slack.url', foo: 'bar' }
+  end
+
 end
