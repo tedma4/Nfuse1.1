dedae6609d1c2e53f59dd214235dca713770f970
tedma4 <tedma4@yahoo.com>
2015-10-07 13:41:31 -0700

    Oops forgot to remove these

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -24,45 +24,37 @@ private
 def post(post_id, provider)
   case(@provider)
     when 'twitter'
-      post_id = params[:post_id]
       token = current_user.tokens.find_by(provider: 'twitter').access_token
       configure_twitter(token.access_token, token.access_token_secret)
       client.status(post_id).map { |post| Twitter::Post.from(post, @user) }
     when 'facebook'
-      post_id = params[:post_id]
       access_token = current_user.tokens.find_by(provider: 'facebook').access_token
       configure_facebook(access_token)
       client.get_object(post_id).map { |post| Facebook::Post.from(post, @user) }
     when 'youtube'
-      post_id = params[:post_id]
       token = current_user.tokens.find_by(provider: 'google_oauth2').access_token
       configure_youtube(token.access_token, token.refresh_token)
       video = Yt::Video.new id: post_id, auth: client
       video.map {|post| Youtube::Post.from(post, @user) }
     when 'gplus'
-      post_id = params[:post_id]
       uid = current_user.tokens.find_by(provider: 'gplus').uid
       configure_gplus(uid)
       post.map { |post| Gplus::Post.from(post, @user) }
     when 'vimeo'
-      post_id = params[:post_id]
       access_token = current_user.tokens.find_by(provider: 'vimeo').access_token
       configure_vimeo(access_token)
       user.videos.find(id: post_id).map { |post| Vimeo::Post.from(post, @user) }
     when 'tumblr'
-      post_id = params[:post_id]
       token = current_user.tokens.find_by(provider: 'tumblr').access_token
       configure_tumblr(token.access_token, token.access_token_secret)
       username = client.info['user']['name']
       client.posts("#{username}.tumblr.com", id: post_id).map { |post| Tumblr::Posting.from(post, @user) }
     when 'flickr'
-      post_id = params[:post_id]
       token = current_user.tokens.find_by(provider: 'flickr').access_token
       configure_flickr(token.access_token, token.access_token_secret)
       info = client.photos.getInfo(:photo_id => post_id)
       FlickRaw.url_z(info).map {|post| Flickr::Post.from(post, @user) }
     when 'instagram'
-      media_id = params[:post_id]
       token = current_user.tokens.find_by(provider: 'instagram')
       instagram_api = Instagram::Api.new(token.access_token, nil)
       instagram_api.get_post(media_id).map { |post| Instagram::Post.from(post, @user) }
