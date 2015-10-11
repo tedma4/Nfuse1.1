6a73a65e6895613e2df0878ef472167d8a35aa42
tedma4 <tedma4@yahoo.com>
2015-10-11 14:45:43 -0700

    Finally got the individual post to match up

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,16 +1,14 @@
 class ActivitiesController < ApplicationController
 	before_action :set_activity, only: :show
 	def show
-		@activity 		= set_activity
-		@post_id      = @activity.parameters[:id]
-		@provider 		= @activity.parameters[:provider]
+		@post_id      = set_activity.parameters[:id]
+		@provider 		= set_activity.parameters[:provider]
 		@activities 	= PublicActivity::Activity.order(created_at: :desc).where(recipient_id: current_user.id,
 																									 recipient_type: 'User')
-		notify        = Notification::Timeline.new(@post_id, @provider, current_user)
-		@post_entry   = notify.construct
-		@post_entry
+		# notify        = Notification::Timeline.new(@post_id, @provider, current_user)
+		# @post_entry   = notify.construct
+		# @post_entry
 	end
-
 	private
 
 	def set_activity
@@ -21,4 +19,4 @@ end
 #
 # @post_id      = @activities.where(trackable_type: 'User').pluck(:parameters).map do |id| {id: id[:id]} end
 # @provider 		= @activities.where(trackable_type: 'User').pluck(:parameters).map do |provider| {provider: provider[:provider]} end
-# I need to associate the post id and provider to the individual Activity
+# I need to associate the post id and provider to the individual Activity. Maybe set each activity id to an array 
