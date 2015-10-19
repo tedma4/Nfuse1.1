c087667d1bac24ff39536dc61e88d09269d75c96
tedma4 <tedma4@yahoo.com>
2015-10-19 15:34:18 -0700

    Removed js making notifications render twice

--- app/assets/javascripts/activities.js
+++ app/assets/javascripts/activities.js
@@ -7,14 +7,14 @@
 //           xhr.setRequestHeader "X-CSRF-Token", $("meta[name=\"csrf-token\"]").attr("content")
 //         cache: false
 
-$(document).on('click', '.cr', function(e) {
-  e.preventDefault();
-  return $.ajax('/activities/read_all_notifications', {
-    type: "post",
-    dataType: "json",
-    beforeSend: function(xhr) {
-      return xhr.setRequestHeader("X-CSRF-Token", $("meta[name=\"csrf-token\"]").attr("content"));
-    },
-    cache: false
-  });
-});
+//$(document).on('click', '.cr', function(e) {
+//  e.preventDefault();
+//  return $.ajax('/activities/read_all_notifications', {
+//    type: "post",
+//    dataType: "json",
+//    beforeSend: function(xhr) {
+//      return xhr.setRequestHeader("X-CSRF-Token", $("meta[name=\"csrf-token\"]").attr("content"));
+//    },
+//    cache: false
+//  });
+//});
