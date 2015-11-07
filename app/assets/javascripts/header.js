a64d2ae11f05b14cbbc04faa211bb688dc4add98
tedma4 <tedma4@yahoo.com>
2015-11-07 11:42:07 -0700

    Updated timline cache key

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,4 +1,4 @@
-$(document).on "page:change"(function(){
+$(document).on "page:load"(function(){
 	$('.menuDropdown').hide();
 	$('.dropdown-toggle').click(function () {
 		$('.menuDropdown').toggle(300);
