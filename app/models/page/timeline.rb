bda3830410c6fa5261df905013df7d1638c2dc7a
tedma4 <tedma4@yahoo.com>
2015-11-10 13:45:19 -0700

    Added bulk of new typhoeus thing to page timeline module

--- app/models/page/timeline.rb
+++ app/models/page/timeline.rb
@@ -13,26 +13,85 @@ module Page
       tw = twitter_setup
       yt = youtube_setup
       ig = instagram_setup
-      PageConcatenator.merge(tw, yt, ig)
+      merge = (tw + yt + ig).sort_by { |post| post.created_time }.reverse
     end
+
+
+
+# require 'thread'
+
+# client = Instagram.client(:access_token => session[:access_token])
+# @user = client.user
+# @recent_media_items = client.user_recent_media
+
+# threads = []
+# threads << Thread.new { @lv = client.tag_recent_media('lv', options = {:count => 60}) }
+# threads << Thread.new { @lv1 = client.tag_recent_media('lv1', options = {:count => 60}) }
+# threads << Thread.new { @lv2 = client.tag_recent_media('lv2', options = {:count => 60}) }
+# threads << Thread.new { @lv3 = client.tag_recent_media('lv3', options = {:count => 60}) }
+# threads.each(&:join) # this waits for all the threads to finish before proceeding
+# puts [@lv, @lv1, @lv2, @lv3]
+
+    
+# def first_concurrency_test
+#   twitter_token
+#   youtube_token
+#   channel = Yt::Channel.new url: @comp_url
+#   instagram_token
+#   thing = Oj.load(Faraday.get("https://api.instagram.com/v1/users/search?q=#{@incomp}&client_id=#{client_id}").body)
+#
+#   # if thing['data'][0]['username'] == @incomp
+#     usid = thing['data'][0]['id']
+#     thingy = Oj.load(Faraday.get("https://api.instagram.com/v1/users/#{usid}/media/recent/?client_id=#{client_id}&count=1").body)
+#   # posts['data'].map { |post| Page::Post.from(post,'instagram') }
+#   #  else
+#   #    []
+#   # end
+#   hydra = Typhoeus::Hydra.hydra
+#   requests = {
+#   twitter_request = Typhoeus::Request.new(client.user_timeline(@comp).take(15)),
+#   youtube_request = Typhoeus::Request.new(channel.videos.first(15)),
+#   instagram_request = Typhoeus::Request.new(thingy)
+#
+#   hydra.queue twitter_request
+#   hydra.queue youtube_request
+#   hydra.queue instagram_request
+#   }
+#   hydra.run # this is a blocking call that returns once all requests are complete
+#   requests.map { |request| request.response.body }
+# end
+
+# hydra = Typhoeus::Hydra.new
+# requests = 10.times.map {
+#   request = Typhoeus::Request.new("www.example.com", followlocation: true)
+#   hydra.queue(request)
+#   request
+# }
+# hydra.run
+
+# responses = requests.map { |request|
+#   request.response.body
+# }
+
+
 
    private
  
     def twitter_setup
       #twitter_posts = []
-     client = Twitter::REST::Client.new do |i|
-       i.consumer_key = ENV['twitter_api_key']
-       i.consumer_secret = ENV['twitter_api_secret']
-     end
-     posts = client.user_timeline(@comp).take(15)
-     posts.map { |post| Page::Post.from(post, 'twitter') }
+      client = Twitter::REST::Client.new do |i|
+        i.consumer_key = ENV['twitter_api_key']
+        i.consumer_secret = ENV['twitter_api_secret']
+      end
+      posts = client.user_timeline(@comp).take(15)
+      posts.map { |post| Page::Post.from(post, 'twitter') }
     end
  
     def youtube_setup
-     Yt.configuration.api_key = ENV['youtube_dev_key']
-     channel = Yt::Channel.new url: @comp_url
-     posts = channel.videos.first(15)
-     posts.map { |post| Page::Post.from(post, 'youtube') }
+      Yt.configuration.api_key = ENV['youtube_dev_key']
+      channel = Yt::Channel.new url: @comp_url
+      posts = channel.videos.first(15)
+      posts.map { |post| Page::Post.from(post, 'youtube') }
     end
 
     def instagram_setup
@@ -46,11 +105,26 @@ module Page
          []
       end
     end
+
+    # def twitter_token
+    #   client = Twitter::REST::Client.new do |i|
+    #     i.consumer_key = ENV['twitter_api_key']
+    #     i.consumer_secret = ENV['twitter_api_secret']
+    #   end
+    # end
+    #
+    # def instagram_token
+    #   client_id = ENV['instagram_client_id']
+    # end
+    #
+    # def youtube_token
+    #   Yt.configuration.api_key = ENV['youtube_dev_key']
+    # end
 	end
 end
 
-class PageConcatenator
-  def self.merge(twitter_setup, youtube_setup, instagram_setup)
-    (twitter_setup + youtube_setup + instagram_setup).sort_by { |post| post.created_time }.reverse
-  end
-end
+# class PageConcatenator
+#   def self.merge(twitter_setup, youtube_setup, instagram_setup)
+#     (twitter_setup + youtube_setup + instagram_setup).sort_by { |post| post.created_time }.reverse
+#   end
+# end
