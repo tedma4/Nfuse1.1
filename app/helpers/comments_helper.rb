124bb51b124f2e5b78f367bdb9f7ac6002efb519
Ted Martinez <tedma4@yahoo.com>
2016-02-10 07:19:22 -0700

    Page forum base is good to go

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,8 +1,8 @@
 module CommentsHelper
   def nested_comments(comments)
     comments.map do |comment, sub_comments|
-      # content_tag(:div, render(comment))
-      render(comment) + content_tag(:div, nested_comments(sub_comments), :class => "nested_messages")
+      content_tag(:div, render(comment))
+      #render(comment) + content_tag(:div, nested_comments(sub_comments), :class => "nested_messages")
     end.join.html_safe
   end
 end
