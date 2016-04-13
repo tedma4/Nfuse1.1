19b9ed7264cbd8a26d7afa6a235ec2b7c860ee4a
Ted Martinez <tedma4@yahoo.com>
2016-04-13 06:38:49 -0700

    Reverted back to old sign up and login

--- app/assets/javascripts/nfuse.js
+++ app/assets/javascripts/nfuse.js
@@ -20,15 +20,15 @@
     });
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
-    $(function() {
-        $('a.page-scroll').bind('click', function(event) {
-            var $anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $($anchor.attr('href')).offset().top
-            }, 1500, 'easeInOutExpo');
-            event.preventDefault();
-        });
-    });
+    // $(function() {
+    //     $('a.page-scroll').bind('click', function(event) {
+    //         var $anchor = $(this);
+    //         $('html, body').stop().animate({
+    //             scrollTop: $($anchor.attr('href')).offset()
+    //         }, 1500, 'easeInOutExpo');
+    //         event.preventDefault();
+    //     });
+    // });
 
 // JQUERY RESPONSIVE
 
