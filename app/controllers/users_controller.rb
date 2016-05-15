dfcfa4126eccbe4bc8325056936d9f89d52b46f2
Ted Martinez <tedma4@yahoo.com>
2016-05-15 16:39:34 -0700

    Removed unneeded link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,11 +27,11 @@ class UsersController < ApplicationController
   end
 
   def destroyuser
-    User.destroy_all
-    Page.destroy_all
-    require 'rake'
-    Nfuse11::Application.load_tasks # <-- MISSING LINE
-    Rake::Task['db:seed'].invoke
+    # User.destroy_all
+    # Page.destroy_all
+    # require 'rake'
+    # Nfuse11::Application.load_tasks # <-- MISSING LINE
+    # Rake::Task['db:seed'].invoke
   end
 
   def remove_token
