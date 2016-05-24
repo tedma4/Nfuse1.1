706c868463ef87e4d2ad441444161cad017192e8
Ted Martinez <tedma4@yahoo.com>
2016-05-23 21:16:22 -0700

    Added if around carosel and uncommented force ssl

--- app/assets/javascripts/nfuse.js
+++ app/assets/javascripts/nfuse.js
@@ -55,59 +55,61 @@
       if($(this).text() === ''){$(this).remove()}
     });
     
-
-        // $('.responsive.carousel').slick({
-        //   dots: false,
-        //   infinite: false,
-        //   speed: 300,
-        //   slidesToShow: 6,
-        //   slidesToScroll: 1,
-        //   variableWidth: false,
-        //   responsive: [
-        //     {
-        //       breakpoint: 2000,
-        //       settings: {
-        //         slidesToShow: 4,
-        //         slidesToScroll: 1,
-        //         infinite: true,
-        //         dots: true
-        //       }
-        //     },
-        //     {
-        //       breakpoint: 1200,
-        //       settings: {
-        //         slidesToShow: 3,
-        //         slidesToScroll: 1,
-        //         infinite: true,
-        //         dots: true
-        //       }
-        //     },
-        //     {
-        //       breakpoint: 992,
-        //       settings: {
-        //         slidesToShow: 2,
-        //         slidesToScroll: 1
-        //       }
-        //     },
-        //     {
-        //       breakpoint: 768,
-        //       settings: {
-        //         slidesToShow: 1,
-        //         slidesToScroll: 1
-        //       }
-        //     },
-        //     {
-        //       breakpoint: 480,
-        //       settings: {
-        //         slidesToShow: 1,
-        //         slidesToScroll: 1
-        //       }
-        //     }
-        //     // You can unslick at a given breakpoint now by adding:
-        //     // settings: "unslick"
-        //     // instead of a settings object
-        //   ]
-        // });
+    if ($('#thehub').length >= 1) {
+        $('.responsive.carousel').slick({
+          dots: false,
+          infinite: false,
+          speed: 300,
+          slidesToShow: 6,
+          slidesToScroll: 1,
+          variableWidth: false,
+          responsive: [
+            {
+              breakpoint: 2000,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 1200,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+        });
+      
+    }
     });
 
     // Change width value on user resize, after DOM
