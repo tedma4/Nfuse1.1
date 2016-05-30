ef770ce5d31a3846148ed344c329c6569399f5cb
Ted Martinez <tedma4@yahoo.com>
2016-05-30 10:54:49 -0700

    Fixed tumblr video size

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery #with: :exception
-  # force_ssl if :ssl_required?
+  force_ssl if :ssl_required?
   # rescue_from Exception, :with => :handle_exceptions if Rails.env.production? && params[:controller] == 'pages'
 
   include SessionsHelper
